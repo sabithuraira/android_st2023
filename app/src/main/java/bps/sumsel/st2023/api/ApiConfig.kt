95b88950b5c485bc09305caf3c3aaffc6bed887a
mkharis <081332210xxx@gmail.com>
2023-05-24 07:36:41 +0700

    set timeout

--- app/src/main/java/bps/sumsel/st2023/api/ApiConfig.kt
+++ app/src/main/java/bps/sumsel/st2023/api/ApiConfig.kt
@@ -1,15 +1,23 @@
 package bps.sumsel.st2023.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiConfig {
     companion object{
         const val baseUrl = "https://st23.bpssumsel.com/api/"
 
         fun getApiService(): ApiInterface{
+            val okHttpClient: OkHttpClient = OkHttpClient().newBuilder()
+                .connectTimeout(5, TimeUnit.MINUTES)
+                .readTimeout(3, TimeUnit.MINUTES)
+                .writeTimeout(1, TimeUnit.MINUTES)
+                .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             return retrofit.create(ApiInterface::class.java)
